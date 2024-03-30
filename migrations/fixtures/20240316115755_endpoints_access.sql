15a7a497f126f45a246f4aa40881fbb4ac5e2068
Andrey Starikov <avstarikov@avito.ru>
2024-03-30 20:44:07 +1000

    fixtures for ConnectChat

--- migrations/fixtures/20240316115755_endpoints_access.sql
+++ migrations/fixtures/20240316115755_endpoints_access.sql
@@ -3,7 +3,8 @@ INSERT INTO endpoint_accesses (endpoint, role, updated_at, created_at)
 VALUES ('/chat_v1.ChatV1/Create', 'admin', now(), now()),
        ('/chat_v1.ChatV1/Delete', 'admin', now(), now()),
        ('/chat_v1.ChatV1/SendMessage', 'admin', now(), now()),
-       ('/chat_v1.ChatV1/SendMessage', 'user', now(), now());
+       ('/chat_v1.ChatV1/SendMessage', 'user', now(), now()),
+       ('/chat_v1.ChatV1/ConnectChat', 'user', now(), now());
 
 -- +goose Down
 truncate table endpoint_accesses;
